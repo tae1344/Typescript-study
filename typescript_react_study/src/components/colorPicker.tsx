a93a320f7e6074d5ebb4fb1207ad36f48c0ca68e
김태연 <xodus1344.hello@gmail.com>
2021-04-02 16:29:57 +0900

    color picker refactoring

--- typescript_react_study/src/components/colorPicker.tsx
+++ typescript_react_study/src/components/colorPicker.tsx
@@ -6,21 +6,49 @@ interface Props {
   onColorUpdated: (color: Color) => void;
 }
 
+interface PropsColorSlider {
+  value: number;
+  onValueUpdated: (newValue: number) => void;
+}
+
+const updateColor = (props: Props, colorId: keyof Color) => (value) => {
+  props.onColorUpdated({
+    ...props.color, // 현재 props.color 객체의 복사본
+    [colorId]: value, // 추가되는 색상 배열값
+  });
+};
+
+const ColorSliderComponent = (props: PropsColorSlider) => {
+  return (
+    <div>
+      <input
+        type="range"
+        min="0"
+        max="255"
+        value={props.value}
+        onChange={(event) => props.onValueUpdated(+event.target.value)}
+      />
+      {props.value}
+    </div>
+  );
+};
+
 export const ColorPicker = (props: Props) => (
-  <div>
-    <input
-      type="range"
-      min="0"
-      max="255"
-      value={props.color.red}
-      onChange={(event) =>
-        props.onColorUpdated({
-          red: +event.target.value,
-          green: props.color.green,
-          blue: props.color.blue,
-        })
-      }
-    />
-    {props.color.red}
-  </div>
+  // 리팩토링 1
+  //   <div>
+  //     <ColorSliderComponent value={props.color.red} onValueUpdated={updateColor(props, "red")} />
+  //     <br />
+  //     <ColorSliderComponent value={props.color.green} onValueUpdated={updateColor(props, "green")} />
+  //     <br />
+  //     <ColorSliderComponent value={props.color.blue} onValueUpdated={updateColor(props, "blue")} />
+  //     <br />
+  //   </div>
+
+  // 리팩토링 2
+  <>
+    {Object.keys(props.color).map((field: keyof Color) => {
+      console.log("key ::: ", Object.keys(props.color));
+      return <ColorSliderComponent key={field} value={props.color[field]} onValueUpdated={updateColor(props, field)} />;
+    })}
+  </>
 );
